15de64dd720b931706b25207d6aa2d9c33641e90
racgoo <lhsung98@naver.com>
2025-01-02 07:48:18 +0900

    feat(Inquire): 문의 정보 컴포넌트 추가

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -1,9 +1,11 @@
 import ProcessInfo from "@components/guide/ProcessInfo";
 import AppDownload from "@components/market/AppDownload";
+import InquireInfo from "@components/inquire/InquireInfo";
 
 function FAQPage() {
   return (
     <div>
+      <InquireInfo />
       <ProcessInfo />
       <AppDownload />
     </div>
