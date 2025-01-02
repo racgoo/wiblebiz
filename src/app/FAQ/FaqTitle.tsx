6d40d007d632384e9c8b5d7162758999b3de1c20
racgoo <lhsung98@naver.com>
2025-01-02 12:33:08 +0900

    fix(faq-title): FaqTitle 컴포넌트 반응형 대응

--- src/app/FAQ/FaqTitle.tsx
+++ src/app/FAQ/FaqTitle.tsx
@@ -1,6 +1,24 @@
+"use client";
+
 import CommonText from "@components/text/CommonText";
+import useMedia, { MediaType } from "@hooks/useMedia";
+
+const titleFontSize: Record<MediaType, number> = {
+  loading: 24,
+  mobile: 24,
+  tablet: 40,
+  pc: 48,
+};
+
+const subTitleFontSize: Record<MediaType, number> = {
+  loading: 14,
+  mobile: 14,
+  tablet: 14,
+  pc: 16,
+};
 
 function FaqTitle() {
+  const media = useMedia();
   return (
     <section
       style={{
@@ -12,10 +30,10 @@ function FaqTitle() {
         height: 124,
       }}
     >
-      <CommonText type="large" size={24}>
+      <CommonText type="large" size={titleFontSize[media]}>
         자주 묻는 질문
       </CommonText>
-      <CommonText type="small" size={14}>
+      <CommonText type="small" size={subTitleFontSize[media]}>
         궁금하신 내용을 빠르게 찾아보세요.
       </CommonText>
     </section>
