efd3e1e82c86998d13f1f13e444574185ece3488
racgoo <lhsung98@naver.com>
2025-01-02 12:06:23 +0900

    feat(Tab): 공용 Tab 컴포넌트 제작

--- src/app/FAQ/Faq.tsx
+++ src/app/FAQ/Faq.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Search from "@components/search/Search";
-import CommonText from "@components/text/CommonText";
 import { useEffect, useState } from "react";
 import FaqTitle from "./FaqTitle";
+import Tab from "@components/tab/Tab";
 
 function Faq() {
   const [searchText, setSearchText] = useState("");
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
   useEffect(() => {
     console.log(searchText);
   }, [searchText]);
@@ -18,6 +19,11 @@ function Faq() {
   return (
     <article>
       <FaqTitle />
+      <Tab
+        tabList={["전체", "회원", "예약", "결제", "취소", "기타"]}
+        setSelectedTabIndex={setSelectedTabIndex}
+        selectedTabIndex={selectedTabIndex}
+      />
       <Search setSearchText={setSearchText} searchAction={searchAction} />
     </article>
   );
